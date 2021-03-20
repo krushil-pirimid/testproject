cfca8d753613d9cd19236be27f5bc7edcbb194b8
kdhameli <krushil@pirimidtech.com>
2021-03-21 00:06:00 +0530

    Initial Test Project for Android Dynamic Links with Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import firebase from 'firebase/app';
+import { FirebaseDynamicLinks } from '@ionic-native/firebase-dynamic-links/ngx';
+import {Platform} from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,42 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+
+  constructor(
+    private firebaseDynamicLinks: FirebaseDynamicLinks,
+    private platform : Platform,
+    ) {
+    const firebaseConfig: any = {
+      apiKey: 'AIzaSyCVDOmOEBn4sOeUaDxgRpvSi-zeNvSIGhY',
+      authDomain: 'testproject-3ffb7.firebaseapp.com',
+      projectId: 'testproject-3ffb7',
+      storageBucket: 'testproject-3ffb7.appspot.com',
+      messagingSenderId: '1056678736562',
+      appId: '1:1056678736562:web:444e28c82c8ac2bc5e0fab',
+      measurementId: 'G-LR3EYEQQWJ'
+    };
+
+    firebase.initializeApp( firebaseConfig );
+
+    this.firebaseDynamicLinks.onDynamicLink()
+      .subscribe((res: any) => {
+        console.log(`dynamic links ${JSON.stringify(res)}`);
+      }, (error: any) => {
+        console.log(`dynamic links ${error}`);
+      });
+
+    this.firebaseDynamicLinks.createShortDynamicLink( {
+      link: 'https://testApp.pirimid.com/tabs',
+      domainUriPrefix: 'https://pirimid.page.link/',
+      androidInfo: {
+        androidPackageName: 'com.pirimid.testApp',
+        androidFallbackLink: 'https://testApp.pirimid.com/tabs',
+        androidMinPackageVersionCode: 0,
+      },
+    } ).then( res => {
+      console.log(`dynamic links ${JSON.stringify(res)}`);
+    }).catch(error => {
+      console.log(`dynamic links ${error}`);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FirebaseDynamicLinks} from '@ionic-native/firebase-dynamic-links/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, FirebaseDynamicLinks],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
